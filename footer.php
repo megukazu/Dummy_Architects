f798e75fabfe7daed62f7513d38e56ed69333073
megukazu <demoganbarou@gmail.com>
2021-03-15 12:37:17 +0900

    functions.php の設定

--- footer.php
+++ footer.php
@@ -1,3 +1,2 @@
 <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-<script src="<?php echo esc_url(get_template_directory_uri()); ?>/dist/js/main.js"></script>
 <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -3,9 +3,7 @@
 <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE=Edge">
 <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
-<link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-<link href="<?php echo esc_url(get_template_directory_uri()); ?>/dist/css/main.css" rel="stylesheet">
 <!-- facebookのOGPタグ -->
 <meta property="og:site_name" content="Dummy Architects" />
 <meta property="og:url" content="" />
