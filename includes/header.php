44e9dcc3f51a6fe1e957ee8b434100f7ef0a3653
megukazu <demoganbarou@gmail.com>
2021-03-24 04:51:14 +0900

    メニューに画像追加

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,7 @@
 <header class="l-header js-header">
 	<div class="l-container__full">
 		<div class="p-header">
-			<a href="<?php echo home_url() ?>">
-				<h1 class="p-header__title">Dummy<br>Architects</h1>
-			</a>
+			<h1 class="p-header__title"> <a href="<?php echo home_url() ?>">Dummy<br>Architects</a></h1>
 			<nav class="l-header__nav" role="navigation">
 				<h2 class="screen-reader-text">サイト内メニュー</h2>
 				<button type="button" class="js-button-hamburger c-button p-button--hamburger" aria-controls="global-nav" aria-expanded="false">
@@ -11,15 +9,18 @@
 						<span class="screen-reader-text">メニューを開閉する</span>
 					</span>
 				</button>
-				<ul id="global-nav" class="p-globalNav">
-					<?php wp_nav_menu(array(
-						'theme_location' => 'place_global',
-						'container'     => '',
-						'menu_class'    => '',
-						'items_wrap' => '%3$s', //<ul>を出力しない
-					));
-					?>
-				</ul>
+				<div class="c-gridFlex p-globalNav__wrap">
+					<div class="c-gridFlex__item -tab6Of12 p-globalNav__thumbnail --menu"></div>
+					<ul id="global-nav" class="p-globalNav c-gridFlex__item -tab6Of12">
+						<?php wp_nav_menu(array(
+							'theme_location' => 'place_global',
+							'container'     => '',
+							'menu_class'    => '',
+							'items_wrap' => '%3$s', //<ul>を出力しない
+						));
+						?>
+					</ul>
+				</div>
 			</nav>
 		</div>
 	</div>
