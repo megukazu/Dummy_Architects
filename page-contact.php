ce92f4265dabc936514e54dc56fd0cdee72e5b54
megukazu <demoganbarou@gmail.com>
2021-03-26 01:38:20 +0900

    オープニングアニメーション実装

--- page-contact.php
+++ page-contact.php
@@ -6,33 +6,33 @@
 </head>
 
 <body>
-    <?php get_template_part('includes/header'); ?>
-    <main>
-        <section class="l-section p-fixedBackground --contact">
-            <div class="l-container__middle --vertical js-headerFloatTrigger">
-                <h2 class="p-heading__primary --center">Contact</h2>
-            </div>
-        </section>
-
-        <section class="l-section p-section">
-            <div class="l-container__middle">
-                <div class="p-form__body">
-                    <p class="p-contents__text">お問い合わせは下記をご記入のうえ、<br>送信してください。</p>
+    <div class="p-body__inner">
+        <?php get_template_part('includes/header'); ?>
+        <main>
+            <section class="l-section p-fixedBackground --contact">
+                <div class="l-container__middle --vertical js-headerFloatTrigger">
+                    <h2 class="p-heading__primary --center">Contact</h2>
                 </div>
-                <?php if (have_posts()) : ?>
-                    <?php while (have_posts()) : the_post(); ?>
-                        <?php remove_filter('the_content', 'wpautop'); ?>
-                        <?php the_content(); ?>
-                    <?php endwhile; ?>
-                <?php else : ?>
-                    <p>投稿がありません。</p>
-                <?php endif; ?>
-            </div>
-        </section>
-
+            </section>
+            <section class="l-section p-section">
+                <div class="l-container__middle">
+                    <div class="p-form__body">
+                        <p class="p-contents__text">お問い合わせは下記をご記入のうえ、<br>送信してください。</p>
+                    </div>
+                    <?php if (have_posts()) : ?>
+                        <?php while (have_posts()) : the_post(); ?>
+                            <?php remove_filter('the_content', 'wpautop'); ?>
+                            <?php the_content(); ?>
+                        <?php endwhile; ?>
+                    <?php else : ?>
+                        <p>投稿がありません。</p>
+                    <?php endif; ?>
+                </div>
+            </section>
+        </main>
         <?php get_template_part('includes/footer'); ?>
         <?php get_footer(); ?>
-    </main>
+    </div>
 </body>
 
 </html>
