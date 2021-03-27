e285e6a8a165aea6c01b6575ebc36dcaa0a80c11
megukazu <demoganbarou@gmail.com>
2021-03-27 22:34:13 +0900

    404ページのリンク先を修正

--- 404.php
+++ 404.php
@@ -13,7 +13,7 @@
                 <div class="l-container__middle js-headerFloatTrigger">
                     <h2 class="p-heading__news --center">お探しのページは見つかりませんでした。</h2>
                 </div>
-                <p class="l-button js-appearItem up"><a class="c-button p-button" href="<?php echo home_url('/news'); ?>"><span class="screen-reader-text">ホームに戻る</span>Home</a></p>
+                <p class="l-button js-appearItem up"><a class="c-button p-button" href="<?php echo home_url('/'); ?>"><span class="screen-reader-text">ホームに戻る</span>Home</a></p>
             </section>
         </main>
         <?php get_template_part('includes/footer'); ?>
